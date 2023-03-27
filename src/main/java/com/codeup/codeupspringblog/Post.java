5551da85827c5e657366404354acf72b356545a3
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-03-27 15:35:55 -0500

    messages

---
+++ src/main/java/com/codeup/codeupspringblog/Post.java
@@ -0,0 +1,41 @@
+package com.codeup.codeupspringblog;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String body;
+
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,29 +1,85 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String postsIndex() {
+    public String index(Model model) {
+        List<Post> posts = new ArrayList<>(); // create an empty list of posts
+        posts.add(new Post(1L, "Sample Post 1", "This is the body of sample post 1."));
+        posts.add(new Post(2L, "Sample Post 2", "This is the body of sample post 2."));
+        model.addAttribute("posts", posts);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String viewPost(@PathVariable long id, Model model) {
-        // Retrieve post with given ID from database and add it to model
+    public String show(@PathVariable long id, Model model) {
+        Post post = new Post(id, "Sample Post", "This is the body of a sample post.");
+        model.addAttribute("post", post);
         return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    public String viewCreateForm() {
+    public String createForm(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
-    // Handle form submission for creating a new post
-    // Redirect to "/posts/{id}" after post is created
+    @PostMapping("/posts")
+    public String create(@ModelAttribute Post post) {
+        // Add your logic to save the post and redirect as needed
+        return "redirect:/posts";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.codeup.codeupspringblog.controllers;
+//
+//package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//@Controller
+//public class PostController {
+//
+//    @GetMapping("/posts")
+//    public String postsIndex() {
+//        return "posts/index";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    public String viewPost(@PathVariable long id, Model model) {
+//        // Retrieve post with given ID from database and add it to model
+//        return "posts/show";
+//    }
+//
+//    @GetMapping("/posts/create")
+//    public String viewCreateForm() {
+//        return "posts/create";
+//    }
+//
+//    // Handle form submission for creating a new post
+//    // Redirect to "/posts/{id}" after post is created
+//}
+//
