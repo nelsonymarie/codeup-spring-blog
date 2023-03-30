6f949ae2d49c5d2a3b49730a06cb4d108e72ce0b
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-03-30 05:19:07 -0500

    con vi rep rel

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,85 +1,105 @@
-package com.codeup.codeupspringblog.controllers;
+//package com.codeup.codeupspringblog.controllers;
+//
+////import com.codeup.codeupspringblog.models.Post;
+//import com.codeup.codeupspringblog.Post;
+//import com.codeup.codeupspringblog.repositories.PostRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//public class PostController {
+//
+//    private final PostRepository postDao;
+//
+//    @Autowired
+//    public PostController(PostRepository postDao) {
+//        this.postDao = postDao;
+//    }
+//
+//    @GetMapping("/posts")
+//    public String index(Model model) {
+//        model.addAttribute("posts", postDao.findAll());
+//        return "posts/index";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    public String show(@PathVariable long id, Model model) {
+//        model.addAttribute("post", postDao.getOne(id));
+//        return "posts/show";
+//    }
+//
+//    @GetMapping("/posts/create")
+//    public String showCreateForm(Model model) {
+//        model.addAttribute("post", new Post());
+//        return "posts/create";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    public String create(@ModelAttribute Post post) {
+//        postDao.save(post);
+//        return "redirect:/posts";
+//    }
+//}
 
-import com.codeup.codeupspringblog.Post;
+
+
+package com.codeup.codeupspringblog.controllers;
+//import com.codeup.codeupspringblog.models.Post;
+
+import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.models.User;
+import com.codeup.codeupspringblog.repositories.PostRepository;
+import com.codeup.codeupspringblog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 public class PostController {
 
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+
+    @Autowired
+    public PostController(PostRepository postDao, UserRepository userDao) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+    }
+
     @GetMapping("/posts")
     public String index(Model model) {
-        List<Post> posts = new ArrayList<>(); // create an empty list of posts
-        posts.add(new Post(1L, "Sample Post 1", "This is the body of sample post 1."));
-        posts.add(new Post(2L, "Sample Post 2", "This is the body of sample post 2."));
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model model) {
-        Post post = new Post(id, "Sample Post", "This is the body of a sample post.");
-        model.addAttribute("post", post);
+        model.addAttribute("post", postDao.getOne(id));
         return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    public String createForm(Model model) {
+    public String showCreateForm(Model model) {
         model.addAttribute("post", new Post());
         return "posts/create";
     }
 
-    @PostMapping("/posts")
+    @PostMapping("/posts/create")
     public String create(@ModelAttribute Post post) {
-        // Add your logic to save the post and redirect as needed
+        List<User> users = userDao.findAll();
+        Random random = new Random();
+        User randomUser = users.get(random.nextInt(users.size()));
+        post.setUser(randomUser);
+        postDao.save(post);
         return "redirect:/posts";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//package com.codeup.codeupspringblog.controllers;
-//
-//package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//@Controller
-//public class PostController {
-//
-//    @GetMapping("/posts")
-//    public String postsIndex() {
-//        return "posts/index";
-//    }
-//
-//    @GetMapping("/posts/{id}")
-//    public String viewPost(@PathVariable long id, Model model) {
-//        // Retrieve post with given ID from database and add it to model
-//        return "posts/show";
-//    }
-//
-//    @GetMapping("/posts/create")
-//    public String viewCreateForm() {
-//        return "posts/create";
-//    }
-//
-//    // Handle form submission for creating a new post
-//    // Redirect to "/posts/{id}" after post is created
-//}
-//
---
+++ src/main/java/com/codeup/codeupspringblog/models/User.java
@@ -0,0 +1,22 @@
+package com.codeup.codeupspringblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Add getters and setters for the fields
+}
